4dc433d563916f2ce68f69fe252695938728fcb2
Heiko Seeberger <hseeberger@users.noreply.github.com>
2023-10-19 17:03:08 +0200

    feat: overlays instead of single environment (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -3,39 +3,38 @@ use serde::Deserialize;
 use std::env;
 use thiserror::Error;
 
-/// Environment variable for the config directory; default is `"config"`.
+/// Environment variable for the configuration directory; default is `"config"`.
 pub const CONFIG_DIR: &str = "CONFIG_DIR";
 
-/// Environment variable for the optional environment (e.g. "prod" or "dev") configuration file.
-pub const CONFIG_ENVIRONMENT: &str = "CONFIG_ENVIRONMENT";
+/// Environment variable for the optional overlays (e.g. "prod" or "dev").
+pub const CONFIG_OVERLAYS: &str = "CONFIG_OVERLAYS";
 
-/// Environment variable for the prefix of environment variable overrides
+/// Environment variable for the prefix of environment variable overrides.
 pub const CONFIG_ENV_PREFIX: &str = "CONIFG_ENV_PREFIX";
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("Cannot load configuration")]
-    Load { source: ConfigError },
+    #[error("cannot load configuration")]
+    Load(#[source] ConfigError),
 
-    #[error("Cannot deserialize configuration")]
-    Deserialize { source: ConfigError },
+    #[error("cannot deserialize configuration")]
+    Deserialize(#[source] ConfigError),
 }
 
 pub trait Configured: Sized {
     /// Opinionated utility to load a configuration from three well defined layers into any type
     /// which can be deserialized by [Serde](https://serde.rs/) using kebab-case.
     ///
-    /// First, values from the mandatory default configuration file at `<CONFIG_DIR>/default.toml`
+    /// First, values from the mandatory default configuration file at `<CONFIG_DIR>/default.yaml`
     /// are loaded.
     ///
-    /// Then, if if the environment variable `CONFIG_ENVIRONMENT` is defined, values from the
-    /// environment (e.g. "prod") specific configuration file at
-    /// `<CONFIG_DIR>/<CONFIG_ENVIRONMENT>.toml` are loaded as an overlay, i.e. adding or
-    /// overwriting already existing values.
+    /// Then, if the environment variable `CONFIG_OVERLAYS` is defined, its comma separated overlays
+    /// (e.g. "prod" or "feat, dev") at `<CONFIG_DIR>/<overlay>.yaml` are loaded from left to right
+    /// as overlays, i.e. adding or overwriting already existing values.
     ///
     /// Finally environment variables prefixed with `<CONFIG_ENV_PREFIX>__` and segments separated
-    /// by `__` (double underscores are used as segment separators to allow for single
-    /// underscores in segment names) are used as additional overlay.
+    /// by `__` (double underscores are used as segment separators to allow for single underscores
+    /// in segment names) are used as final overlay.
     fn load() -> Result<Self, Error>;
 }
 
@@ -45,20 +44,28 @@ where
 {
     fn load() -> Result<Self, Error> {
         let config_dir = env::var(CONFIG_DIR).unwrap_or_else(|_| "config".to_string());
-        let config_env_prefix = env::var(CONFIG_ENV_PREFIX).unwrap_or_else(|_| "app".to_string());
-        env::var(CONFIG_ENVIRONMENT)
-            .iter()
+        let config_env_prefix = env::var(CONFIG_ENV_PREFIX).unwrap_or_else(|_| "APP".to_string());
+        env::var(CONFIG_OVERLAYS)
+            .into_iter()
+            .flat_map(|overlays| {
+                overlays
+                    .split(',')
+                    .map(|s| s.trim().to_owned())
+                    .collect::<Vec<_>>()
+            })
             .fold(
                 Config::builder().add_source(File::with_name(&format!("{config_dir}/default"))),
-                |config, env| config.add_source(File::with_name(&format!("{config_dir}/{env}"))),
+                |config, overlay| {
+                    config.add_source(File::with_name(&format!("{config_dir}/{overlay}")))
+                },
             )
             .add_source(KebabEnvironment(
                 Environment::with_prefix(&config_env_prefix).separator("__"),
             ))
             .build()
-            .map_err(|source| Error::Load { source })?
+            .map_err(Error::Load)?
             .try_deserialize()
-            .map_err(|source| Error::Deserialize { source })
+            .map_err(Error::Deserialize)
     }
 }
 
@@ -106,21 +113,23 @@ mod tests {
     #[derive(Debug, Deserialize)]
     #[serde(rename_all = "kebab-case")]
     struct Qux {
-        quux_quux: String,
+        quux: String,
+        corge_grault: String,
     }
 
     #[test]
-    fn test_load() {
+    fn test_load() -> Result<(), Error> {
         env::set_var(CONFIG_DIR, "test-config");
-        env::set_var(CONFIG_ENVIRONMENT, "dev");
-        env::set_var("APP__QUX__QUUX-QUUX", "Quux2");
+        env::set_var(CONFIG_OVERLAYS, "feat, dev");
+        env::set_var("APP__QUX__CORGE_GRAULT", "corge-grault-env");
 
-        let config = Config::load();
-        assert!(config.is_ok());
-        let config = config.unwrap();
+        let config = Config::load()?;
 
-        assert_eq!(config.foo.bar.as_str(), "Bar");
-        assert_eq!(config.foo.baz.as_str(), "Baz2");
-        assert_eq!(config.qux.quux_quux.as_str(), "Quux2");
+        assert_eq!(config.foo.bar.as_str(), "bar");
+        assert_eq!(config.foo.baz.as_str(), "baz-dev");
+        assert_eq!(config.qux.quux.as_str(), "quux-feat");
+        assert_eq!(config.qux.corge_grault.as_str(), "corge-grault-env");
+
+        Ok(())
     }
 }
